0da2f71cc0207f14d7216526c926493a28f5e939
Matteo Lisi <matte.crash@gmail.com>
2022-09-07 13:37:29 +0100

    added workshop LMM with links

--- compile_book.R
+++ compile_book.R
@@ -5,7 +5,7 @@ setwd("/mnt/sda2/matteoHDD/git_local_HDD/RHUL-stats/RHUL-stats-notebook")
 
 # bookdown::serve_book(dir="/mnt/sda2/matteoHDD/git_local_HDD/RHUL-stats/RHUL-stats-notebook",
 #                      output_dir = "../docs")
-# servr::daemon_stop(3)
+# servr::daemon_stop(1)
 
 bookdown::render_book("index.Rmd", "bookdown::bs4_book",
                       output_dir = "../docs",
@@ -23,5 +23,5 @@ system("rm *.md")
 system("rm *.tex")
 
 system("git add -A")
-system('git commit -m "misc minor"')
+system('git commit -m "added workshop LMM with links"')
 system("git push origin main")
