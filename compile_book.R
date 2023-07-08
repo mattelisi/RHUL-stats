0b7287298fcd831867eb441885e8da81852c747a
Matteo Lisi <matte.crash@gmail.com>
2023-07-08 11:12:08 +0100

    minor corrections

--- compile_book.R
+++ compile_book.R
@@ -3,9 +3,9 @@
 rm(list=ls())
 setwd("/mnt/sda2/matteoHDD/git_local_HDD/RHUL-stats/RHUL-stats-notebook")
 
-bookdown::serve_book(dir="/mnt/sda2/matteoHDD/git_local_HDD/RHUL-stats/RHUL-stats-notebook",
-                     output_dir = "../docs")
-# servr::daemon_stop(1)
+# bookdown::serve_book(dir="/mnt/sda2/matteoHDD/git_local_HDD/RHUL-stats/RHUL-stats-notebook",
+#                      output_dir = "../docs")
+# # servr::daemon_stop(1)
 
 bookdown::render_book("index.Rmd", "bookdown::bs4_book",
                       output_dir = "../docs",
