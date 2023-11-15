24377164f3a6b9591c8205336ab99a32a95ba06a
matteo <matte.crash@gmail.com>
2023-11-15 10:28:16 +0000

    pdf link added

--- compile_book.R
+++ compile_book.R
@@ -1,7 +1,8 @@
 #!/usr/bin/env Rscript
 
 rm(list=ls())
-setwd("/mnt/sda2/matteoHDD/git_local_HDD/RHUL-stats/RHUL-stats-notebook")
+# setwd("/mnt/sda2/matteoHDD/git_local_HDD/RHUL-stats/RHUL-stats-notebook")
+setwd("~/git_local/RHUL-stats/RHUL-stats-notebook")
 
 # bookdown::serve_book(dir="/mnt/sda2/matteoHDD/git_local_HDD/RHUL-stats/RHUL-stats-notebook",
 #                      output_dir = "../docs")
@@ -11,14 +12,15 @@ bookdown::render_book("index.Rmd", "bookdown::bs4_book",
                       output_dir = "../docs",
                       new_session = TRUE)
 
-bookdown::render_book("index.Rmd", "bookdown::pdf_book",
-                      output_dir = "../",
-                      new_session = TRUE)
+# bookdown::render_book("index.Rmd", "bookdown::pdf_book",
+#                       output_dir = "../",
+#                       new_session = TRUE)
 
 options(bookdown.clean_book = TRUE)
 bookdown::clean_book()
 
-setwd("/mnt/sda2/matteoHDD/git_local_HDD/RHUL-stats/")
+# setwd("/mnt/sda2/matteoHDD/git_local_HDD/RHUL-stats/")
+setwd("~/git_local/RHUL-stats/")
 system("rm *.md")
 system("rm *.tex")
 
