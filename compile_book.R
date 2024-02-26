9c233f27531c79a158bf89447c9b1791aa5993b1
matteo <matte.crash@gmail.com>
2024-02-26 19:27:00 +0000

    added zipf law

--- compile_book.R
+++ compile_book.R
@@ -1,8 +1,8 @@
 #!/usr/bin/env Rscript
 
 rm(list=ls())
-setwd("/mnt/sda2/matteoHDD/git_local_HDD/RHUL-stats/RHUL-stats-notebook")
-# setwd("~/git_local/RHUL-stats/RHUL-stats-notebook")
+# setwd("/mnt/sda2/matteoHDD/git_local_HDD/RHUL-stats/RHUL-stats-notebook")
+setwd("~/git_local/RHUL-stats/RHUL-stats-notebook")
 
 # bookdown::serve_book(dir="/mnt/sda2/matteoHDD/git_local_HDD/RHUL-stats/RHUL-stats-notebook",
 #                      output_dir = "../docs")
@@ -19,11 +19,11 @@ bookdown::render_book("index.Rmd", "bookdown::pdf_book",
 options(bookdown.clean_book = TRUE)
 bookdown::clean_book()
 
-setwd("/mnt/sda2/matteoHDD/git_local_HDD/RHUL-stats/")
-# setwd("~/git_local/RHUL-stats/")
+# setwd("/mnt/sda2/matteoHDD/git_local_HDD/RHUL-stats/")
+setwd("~/git_local/RHUL-stats/")
 system("rm *.md")
 system("rm *.tex")
 
 system("git add -A")
-system('git commit -m "added intro Bayes slides"')
+system('git commit -m "added zipf law"')
 system("git push origin main")
--- docs/index.md
+++ docs/index.md
@@ -1,7 +1,7 @@
 --- 
 title: "RHUL Psychology\nStatistical modelling notebook"
 author: "Matteo Lisi"
-date: "2023-11-28"
+date: "2024-02-26"
 site: bookdown::bookdown_site
 documentclass: book
 always_allow_html: true
